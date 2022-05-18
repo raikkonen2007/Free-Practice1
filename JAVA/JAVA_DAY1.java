0310e2b5e69101b6a9a58e60830b19524b20a384
raikkonen2007 <105337951+raikkonen2007@users.noreply.github.com>
2022-05-19 02:06:55 +0900

    Add files via upload

---
+++ JAVA/JAVA_DAY1.java
@@ -0,0 +1,22 @@
+
+public class DAY1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String A="Hello!";
+		String B="Nice to meet you!";
+		String C="This is my first upload!";
+		String month="May";
+		int day=18;
+		int year=2022;
+		
+		System.out.println(A+" "+B);
+		System.out.println(C);
+		System.out.print(month+"."+day+"th"+"."+year);
+		
+		
+
+	}
+
+}
