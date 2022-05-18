9c0d02ebcd80d6845bc9292abdf64477eda1a54f
raikkonen2007 <105337951+raikkonen2007@users.noreply.github.com>
2022-05-19 02:08:03 +0900

    Add files via upload

---
+++ JAVA/java1.java
@@ -0,0 +1,32 @@
+
+public class java1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=100;
+		int b=210;
+		double c= 3.476;
+		final int MAX = 2022;	
+		
+		char ch='A';
+		String str="JA";
+		String ing="VA";
+		String mos="CLASS";
+		
+		System.out.println(str+"."+ing+"."+mos);
+		
+		
+		int x = 7000 , z = 2575;
+		
+		System.out.println(z+x);
+		System.out.println(z-x);
+		System.out.println(z*x);
+		System.out.println(z/x);
+			
+				
+				
+
+	}
+
+}
