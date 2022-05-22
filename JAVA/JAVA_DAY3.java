283a94c65ee10ad91290ebc12c17b201aad1fec2
raikkonen2007 <105337951+raikkonen2007@users.noreply.github.com>
2022-05-22 16:01:14 +0900

    Update JAVA_DAY3.java

--- JAVA/JAVA_DAY3.java
+++ JAVA/JAVA_DAY3.java
@@ -7,15 +7,15 @@ public class day3 {
 		Scanner scanner = new Scanner(System.in);
 		
 		
-		System.out.println("-���� ver.1-");
-		System.out.println("���ڸ� �Է����ּ���");
+		System.out.println("-계산기 ver.1-");
+		System.out.println("숫자를 입력해주세요");
 		String str1 = scanner.nextLine();
 		//int num1 = scanner.nextInt();
 		
-		System.out.println("��Ģ����");
+		System.out.println("사칙연산");
 		String op = scanner.nextLine();
 		//int op = scanner.nextInt();
-		System.out.println("���ڸ� �Է����ּ���");
+		System.out.println("숫자를 입력해주세요");
 		String str2 = scanner.nextLine();
 		//int num2 = scanner.nextInt();
 		
@@ -36,8 +36,9 @@ public class day3 {
 			num3=num1*num2;
 		}
 		
-		System.out.println(num1+op+num2+"="+num3);
+		System.out.println(num1+op+num2+"="+num3); -> 삭제
 		
+		// num3를 프린트 말고 저장한다음에 다시 프린트ln으로 num3+출력형식
 		
 	}
 
