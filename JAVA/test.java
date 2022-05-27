07a06cc93cf655a93496ff33cb452b11472985f5
raikkonen2007 <105337951+raikkonen2007@users.noreply.github.com>
2022-05-27 22:45:53 +0900

    Add files via upload

--- JAVA/test.java
+++ JAVA/test.java
@@ -1,32 +1,41 @@
+import java.util.Scanner;
 
-public class java1 {
+public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub	//	
 		
-		int a=100;
-		int b=210;
-		double c= 3.476;
-		final int MAX = 2022;	
+		int num1 = '0';
+		int num2 = '0';
+		int num3 = '0';
 		
-		char ch='A';
-		String str="JA";
-		String ing="VA";
-		String mos="CLASS";
-		
-		System.out.println(str+"."+ing+"."+mos);
-		
-		
-		int x = 7000 , z = 2575;
-		
-		System.out.println(z+x);
-		System.out.println(z-x);
-		System.out.println(z*x);
-		System.out.println(z/x);
+		String op = "";
 			
-				
-				
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력해주세요");
+		num1 = scanner.nextInt();
+		
+		System.out.println("연산자를 입력해주세요");
+		op = scanner.next();
+		
+		System.out.println("숫자를 입력해주세요");
+		num2 = scanner.nextInt();
+		
+		if(op.equals("+")) {
+			num3 = num1 + num2;
+		}else if(op.equals("-")){
+			num3 = num1 - num2;
+		}else if(op.equals("*")) {
+			num3 = num1 * num2;
+		}else {
+			num3 = num1 / num2;
+		}
+		
+		System.out.println(num3+"입니다.");
+		System.out.println("num1="+num1);
+		System.out.println("num2="+num2);
+		System.out.println("연산자="+op );
 	}
 
 }
